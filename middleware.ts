31a568a96cb209908ac6b7e4422aee2cb550ef22
Mazen Chaouch <106531325+MazenChaouch@users.noreply.github.com>
2024-11-21 07:55:34 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const middleware = async (req: NextRequest) => {
   if (req.nextUrl.pathname === "/") {
-    return NextResponse.redirect("http://localhost:3000/home");
+    return NextResponse.redirect("https://ecommerce-jam.vercel.app/home");
   }
   return NextResponse.next();
 };
